6253eecacc8516e85ae0d7979666762ec6a1f59b
JON BEREZOWSKI <jberez@live.ca>
2020-12-29 12:47:09 +0100

    Making more separate .jl files

---
+++ helper_functions.jl
@@ -0,0 +1,85 @@
+#Helper Functions
+function plot_data_classifier(data,classes,alpha=1.0)
+    markers = ["o","*"]
+    colors = ["blue","green"]
+    for i=1:2
+        mask = [classes[j] == i for j in 1:length(classes)]
+        scatter(data[:,1][mask],data[:,2][mask],c=colors[i],alpha=alpha,marker=markers[i],zorder=3)
+    end
+end;
+
+function data_labeller(y::Array{Float64})
+    labels = [y[i] > 0.5 ? 2 : 1 for i=1:length(y)]
+    return labels
+end
+
+function trace_acc(trace)
+    pred_y = G(x, trace)
+    pred_labels = data_labeller(pred_y)
+    acc = sum([classes[i] == pred_labels[i] for i=1:length(classes)])
+    return acc
+end
+
+mₖ(k) = k*4 + 1;
+
+function layer_unpacker(i,l,k)
+    if i == 1
+        input_dim = d
+        output_dim = k[i]
+    else
+        input_dim = k[i-1]
+        output_dim = k[i]
+    end
+    return input_dim, output_dim
+end
+
+#Generate XOR Data
+function real_data_classifier(N::Int, modes::Int, σ::Float64)
+    μ₁ = [-0.5, -0.5]
+    μ₂ = [-0.5, 0.5]
+    μ₃ = [0.5, 0.5]
+    μ₄ = [0.5, -0.5]
+    #μ₅ = [1.25, 1.25]
+    #μ₆ = [1.25, 1.75]
+    #μ₇ = [1.75, 1.75]
+    #μ₈ = [1.75, 1.25]
+    μ = [μ₁, μ₂, μ₃, μ₄]
+    Σ = [[σ, 0] [0, σ]]
+    
+    all_samples = zeros(Float64, (N*modes, 2))
+    classes = zeros(Int, (N*modes))
+    
+    for i = 1:modes
+        dist = MvNormal(μ[i], Σ)
+        sample = rand(dist, N)::Matrix
+        #scatter(sample[1,:],sample[2,:])
+        all_samples[(i-1)*N+1:i*N,:] = transpose(sample)
+        classes[(i-1)*N+1:i*N] = fill(i, N)
+        classes = float(classes)
+    end
+    return all_samples, classes
+end;
+
+##################
+#RJ Help Functions
+##################
+
+function select_hyperparameters(trace, obs)
+    args = get_args(trace)
+    argdiffs = map((_) -> NoChange(), args)
+    (new_trace,weight,retdiff) = regenerate(trace, args, argdiffs, select(:τ₁,:τ₂,:τ₃))
+    obs[:τ₁] = new_trace[:τ₁]
+    obs[:τ₂] = new_trace[:τ₂]
+    obs[:τ₃] = new_trace[:τ₃]
+    return new_trace, obs
+end
+
+function select_selection(trace)
+    l = trace[:l]
+    selection = select()
+    for i=1:l+1
+        push!(selection, (:W,i))
+        push!(selection, (:b,i))
+    end
+    return selection
+end
