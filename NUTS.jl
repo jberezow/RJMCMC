27ddfce85dc3268784468df47a461d38ac3fccd6
JON BEREZOWSKI <jberez@live.ca>
2021-01-08 23:17:40 +0100

    Algorithm 6 fully implemented for NUTS, need to spot check, not working

--- NUTS.jl
+++ NUTS.jl
@@ -35,29 +35,37 @@ function find_reasonable_epsilon(trace,selection,vals,θ)
     return ϵ
 end
 
+function L(trace,θ,selection)
+    new_trace = trace
+    args = get_args(new_trace)
+    argdiffs = map((_) -> NoChange(), args)
+    retval_grad = accepts_output_grad(get_gen_fn(trace)) ? zero(get_retval(trace)) : nothing
+    (_, vals, gradient_trie) = choice_gradients(trace, selection, retval_grad)
+    θ_update = from_array(vals, θ)
+    (new_trace, _, _) = update(new_trace, args, argdiffs, θ_update)
+    get_score(new_trace)
+end
+
 #Tree Building for Efficient NUTS
-function build_tree(trace,selection,vals,θ,r,u,v,j,ϵ,θ⁰,r⁰,prev_trace)
+function build_tree(trace,selection,vals,θ,r,u,v,j,ϵ,θ⁰,r⁰)
     #Base case - take one leapfrog step in the direction of v
     if j == 0
         θ¹,r¹,tree_trace = leapfrog(trace,selection,vals,θ,r,v*ϵ)
-        prev_score = get_score(prev_trace) - 0.5(dot(r⁰,r⁰))
+        prev_score = L(trace,θ⁰,selection) - 0.5(dot(r⁰,r⁰))
         score = get_score(tree_trace) - 0.5(dot(r¹,r¹))
-        if u ≤ exp(score)
-            n¹ = 1
-        else
-            n¹ = 0
-        end
-        s¹ = (score > log(u) - Δmax) ? 1 : 0
+
+        n¹ = (log(u) ≤ score) ? 1 : 0
+        s¹ = (log(u) < Δmax + score) ? 1 : 0
         α¹ = min(1, exp(score-prev_score))
         return θ¹,r¹,θ¹,r¹,θ¹,n¹,s¹,α¹,1
     #Recursion - build left and right subtrees
     else
-        θ⁻,r⁻,θ⁺,r⁺,θ¹,n¹,s¹,α¹,nᵅ¹ = build_tree(trace,selection,vals,θ,r,u,v,j-1,ϵ,θ⁰,r⁰,prev_trace)
+        θ⁻,r⁻,θ⁺,r⁺,θ¹,n¹,s¹,α¹,nᵅ¹ = build_tree(trace,selection,vals,θ,r,u,v,j-1,ϵ,θ⁰,r⁰)
         if s¹ == -1
             if v == -1
-                θ⁻,r⁻,_,_,θ²,n²,s²,α²,nᵅ² = build_tree(trace,selection,vals,θ⁻,r⁻,u,v,j-1,ϵ,θ⁰,r⁰,prev_trace)
+                θ⁻,r⁻,_,_,θ²,n²,s²,α²,nᵅ² = build_tree(trace,selection,vals,θ⁻,r⁻,u,v,j-1,ϵ,θ⁰,r⁰)
             else
-                _,_,θ⁺,r⁺,θ²,n²,s²,α²,nᵅ² = build_tree(trace,selection,vals,θ⁺,r⁺,u,v,j-1,ϵ,θ⁰,r⁰,prev_trace)
+                _,_,θ⁺,r⁺,θ²,n²,s²,α²,nᵅ² = build_tree(trace,selection,vals,θ⁺,r⁺,u,v,j-1,ϵ,θ⁰,r⁰)
             end
             met_ind = n²/(n¹+n²)
             if bernoulli(met_ind) == true
@@ -72,7 +80,7 @@ function build_tree(trace,selection,vals,θ,r,u,v,j,ϵ,θ⁰,r⁰,prev_trace)
             s¹ = s²*i¹*i²
             n¹ = n¹ + n²
         end
-       return θ⁻,r⁻,θ⁺,r⁺,θ¹,n¹,s¹,α¹,nᵅ
+       return θ⁻,r⁻,θ⁺,r⁺,θ¹,n¹,s¹,α¹,nᵅ¹
     end
 end;
 
@@ -120,6 +128,7 @@ function NUTS(trace, selection::Selection, ϵ, check, observations, M, Madapt, p
     θ⁰ = to_array(vals, Float64)
     r⁰ = sample_momenta(length(θ⁰))
     θ = θ⁰
+    θᵐ = θ
     r = r⁰
     new_trace = trace
     
@@ -128,13 +137,14 @@ function NUTS(trace, selection::Selection, ϵ, check, observations, M, Madapt, p
     prev_momenta_score = assess_momenta(r⁰)
         
     #Initialize ϵ
-    ϵ = find_reasonable_epsilon(trace,selection,vals,θ⁰)
-    μ = log(10*ϵ)
+    ϵ⁰ = find_reasonable_epsilon(trace,selection,vals,θ⁰)
+    μ = log(10*ϵ⁰);ϵ¹=1;H⁰=0;γ=0.05;t⁰=10;κ=0.75;δ=0.65
     #println("Epsilon: $ϵ")
         
     #Loop M times
     for m=1:M
-        #Resample Position Variables
+        #Resample momentum Variables
+        m == 1 ? (r = r⁰) : (r = sample_momenta(length(θ)))
         params = from_array(vals, θ)
         (new_trace, _, _) = update(new_trace, args, argdiffs, params)
         score = exp(get_score(new_trace) - 0.5(dot(r,r)))
@@ -146,21 +156,21 @@ function NUTS(trace, selection::Selection, ϵ, check, observations, M, Madapt, p
         end
         
         #Initialize
-        θ¹ = θ; θ⁻ = θ; θ⁺ = θ; r⁻ = r; r⁺ = r; j = 0; s = 1; n = 1
+        θ¹ = θ; θ⁻ = θ; θ⁺ = θ; r⁻ = r; r⁺ = r; j = 0; s = 1; n = 1; θᵐ = θ; nᵅ = 1
         
         while s == 1
             vⱼ = rand([-1,1])
             if vⱼ == -1
-                θ⁻,r⁻,_,_,θ¹,n¹,s¹ = build_tree(new_trace,selection,vals,θ⁻,r⁻,u,vⱼ,j,ϵ,θ⁰,r⁰,trace)
+                θ⁻,r⁻,_,_,θ¹,n¹,s¹,α,nᵅ = build_tree(new_trace,selection,vals,θ⁻,r⁻,u,vⱼ,j,ϵ⁰,θ,r)
             else
-                _,_,θ⁺,r⁺,θ¹,n¹,s¹ = build_tree(new_trace,selection,vals,θ⁺,r⁺,u,vⱼ,j,ϵ,θ⁰,r⁰,trace)
+                _,_,θ⁺,r⁺,θ¹,n¹,s¹,α,nᵅ = build_tree(new_trace,selection,vals,θ⁺,r⁺,u,vⱼ,j,ϵ⁰,θ,r)
             end
             if s¹ == 1
                 met_ind = (n¹/n > 1.0) ? 1.0 : n¹/n
                 if bernoulli(met_ind) == true
-                    θ = θ¹
+                    θᵐ = θ¹
                 else
-                    θ = θ
+                    θᵐ = θᵐ
                 end
             end
             n = n + n¹
@@ -169,11 +179,21 @@ function NUTS(trace, selection::Selection, ϵ, check, observations, M, Madapt, p
             s = s¹*i¹*i²
             j += 1
         end
+            
+        if m ≤ Madapt
+            H⁰ = ((1-(1/(m+t₀)))*H⁰)+((1/(m+t₀))*(δ-(α/nᵅ)))
+            ϵ⁰ = exp(μ - (√m)/γ * H⁰)
+            ϵ¹ = exp(m^(-κ)*log(ϵ⁰) + (1-m^(-κ))*log(ϵ¹))
+        else
+            ϵ⁰ = ϵ⁰
+        end
+        #println("Epsilon: $ϵ⁰")
+            
     end
     
-    θ = from_array(vals, θ)
+    θ = from_array(vals, θᵐ)
     momenta = r
-    (new_trace, _, _) = update(trace, args, argdiffs, θ)
+    (new_trace, _, _) = update(new_trace, args, argdiffs, θ)
     
     new_model_score = get_score(new_trace) # assess new model score (negative potential energy)
     new_momenta_score = assess_momenta(-momenta) # assess new momenta score (negative kinetic energy)
